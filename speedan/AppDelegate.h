1edbc0e42969c0a2466e1be7d73a85157dc42095
wangwayhome <wangwayhome@wangwayhomeMacBook-Pro.local>
2015-08-27 16:38:27 +0800

    Initial Commit

---
+++ speedan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  speedan
+//
+//  Created by wangwayhome on 15/8/27.
+//  Copyright (c) 2015年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ speedan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  speedan
+//
+//  Created by wangwayhome on 15/8/27.
+//  Copyright (c) 2015年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
